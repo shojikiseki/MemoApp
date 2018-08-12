2982d3b5cebd3acda2b5f46b4fe2b390c7a2b5e6
Kiseki Shoji <o9o3.kiseki@gmail.com>
2018-08-12 14:46:51 +0900

    eslintの設定

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>hoga</Text>
+        <Text>hello!</Text>
       </View>
     );
   }
