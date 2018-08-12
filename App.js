6c49867d9cb20f760fb2b81089aab3aa9c97cc28
Kiseki Shoji <o9o3.kiseki@gmail.com>
2018-08-12 16:06:42 +0900

    memoList関連のスタイリング

--- App.js
+++ App.js
@@ -42,23 +42,47 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
   },
   appbar: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    height: 70,
+    height: 78,
     paddingTop: 30,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#265366',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
   },
   appbarTitle: {
     color: '#fff',
     fontSize: 18,
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
   },
 });
