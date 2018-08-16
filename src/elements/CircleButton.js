7b3490428b38997fefe734338cab9c2fb2594dc4
Kiseki Shoji <o9o3.kiseki@gmail.com>
2018-08-16 10:50:42 +0900

    CircleButtonのスタイリング上書き

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,10 +3,19 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
-    const { style } = this.props;
+    const { style, color } = this.props;
+
+    let bgColor = '#e31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#e31676';
+    }
+
     return (
-      <View style={[styles.circleButton, style]}>
-        <Text style={styles.circleButtonTitle}>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>
           {this.props.children}
         </Text>
       </View>
@@ -21,7 +30,6 @@ const styles = StyleSheet.create({
     right: 40,
     height: 50,
     width: 50,
-    backgroundColor: '#e31676',
     borderRadius: 24,
     justifyContent: 'center',
     alignItems: 'center',
--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -20,7 +20,7 @@ class MemoDetailScreen extends React.Component {
           <Text>講座のアイデアです</Text>
         </View>
 
-        <CircleButton style={styles.editButton}>+</CircleButton>
+        <CircleButton color="white" style={styles.editButton}>+</CircleButton>
       </View>
     );
   }
