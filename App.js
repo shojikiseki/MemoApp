52760bbae6d51bcc88ad4fcde08c7272e36854b7
Kiseki Shoji <o9o3.kiseki@gmail.com>
2018-09-01 16:17:11 +0900

    firebaseの設定を追加

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StackNavigator } from 'react-navigation';
+import firebase from 'firebase';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+
+import ENV from './env.json';
+
+const config = {
+  apiKey:            ENV.FIREBASE_API_KEY,
+  authDomain:        ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL:       ENV.FIREBASE_DB_URL,
+  projectId:         ENV.FIREBASE_PRJ_ID,
+  storageBucket:     ENV.FIREBASE_STORAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID,
+};
+firebase.initializeApp(config);
 
 const App = StackNavigator({
   Login:      { screen: LoginScreen },
